9cbc7c07e29cc759422bbf24a6b543a49ebd17f7
austynhorne <austynhorne@byui.edu>
2022-03-18 14:37:00 -0600

    Team 5

--- src/js/productList.js
+++ src/js/productList.js
@@ -1,29 +1,36 @@
-export default class ProductListing {
-    constructor(category, dataSource, listElement) {
-      // We passed in this information to make our class as reusable as possible.
-      // Being able to define these things when we use the class will make it very flexible
-      this.category = category;
-      this.dataSource = dataSource;
-      this.listElement = listElement;
-    }
-    async init() {
-      // our dataSource will return a Promise...so we can use await to resolve it.
-      const list = await this.dataSource.getData();
-      // render the list 
-      this.renderList(list);
-    }
-    renderList(list) {
-      const template = document.getElementById('product-card-template');
-      list.forEach(product => {
-        const clone = template.content.cloneNode(true);
-        const hydratedTemplate = this.prepareTemplate(clone, product);
-        this.listElement.appendChild(hydratedTemplate);
-      })
-    }
-    prepareTemplate(template, product) {
-      
-      template.querySelector('a').href +=  product.Id;
-      // fill in the rest of the data here... 
-      return template;
-    }
+import { renderListWithTemplate } from './utils.js';
+
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    
+    this.listElement = listElement;
   }
+  async init() {
+
+    const list = await this.dataSource.getData(this.category);
+    console.log(list);
+    this.renderList(list);
+
+    document.querySelector('.title').innerHTML = this.category;
+  }
+  
+  prepareTemplate(template, product) {
+    
+    template.querySelector('a').href +=  product.Id;
+    template.querySelector('img').src = product.Images.PrimaryMedium;
+    template.querySelector('img').alt += product.Name;
+    template.querySelector('.card__brand').textContent = product.Brand.Name;
+    template.querySelector('.card__name').textContent = product.NameWithoutBrand;
+    template.querySelector('.product-card__price').textContent += product.FinalPrice; 
+    return template;
+  }
+  renderList(list) {
+
+    this.listElement.innerHTML = '';
+
+    const template = document.getElementById('product-card-template');
+    renderListWithTemplate(template, this.listElement, list, this.prepareTemplate);
+    
+  }
+
+}
