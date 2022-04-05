c462909d968c0f85188856ad4e9454545765ddda
austynhorne <austynhorne@byui.edu>
2022-04-04 22:05:30 -0600

    8

--- src/js/ExternalServices.js
+++ src/js/ExternalServices.js
@@ -1,5 +1,5 @@
 const baseURL = 'http://157.201.228.93:2992/';
-// const baseURL = 'http://127.0.0.1:3000/';
+
 async function convertToJson(res) {
   const data = await res.json();
   if (res.ok) {
@@ -9,17 +9,14 @@ async function convertToJson(res) {
   }
 }
 
-export default class ExternalServices {
+export default class ExternalServices  {
   constructor() {
-    //Using the API means we don't need to tie the dataSource to a specific category anymore. So we can remove this in the constructor.
-    // this.category = category;
-    // this.path = `../json/${this.category}.json`;
+   
   }
   getData(category) {
-    // instead we will pass the category we want in here when we need it.
+
     return fetch(baseURL + `products/search/${category}`)
-      .then(convertToJson)
-      .then((data) => data.Result);
+      .then(convertToJson).then((data) => data.Result);
   }
   async findProductById(id) {
     return await fetch(baseURL + `product/${id}`).then(convertToJson)
@@ -29,10 +26,34 @@ export default class ExternalServices {
     const options = {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(payload),
-    };
-    return await fetch(baseURL + 'checkout/', options).then(convertToJson);
+      body: JSON.stringify(payload)
+    }
+      return await fetch(baseURL + 'checkout/', options).then(convertToJson);
+  }
+
+  async loginRequest(user) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    }
+    const response = await fetch(baseURL + 'login', options).then(convertToJson);
+    return response.accessToken;
+  }
+
+  async getOrders(token) {
+    const options = {
+      method: 'GET',
+ 
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }
+    const response = await fetch(baseURL + 'orders', options).then(convertToJson);
+    return response;
   }
 }
