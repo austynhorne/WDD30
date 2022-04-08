4f74c80fd74e3c83f70600829da7a830b07f2824
austynhorne <austynhorne@byui.edu>
2022-04-07 19:20:57 -0600

    test

--- src/js/cart.js
+++ src/js/cart.js
@@ -5,7 +5,7 @@ loadHeaderFooter();
 
 const cart = new CartList('so-cart', document.querySelector('.product-list'));
 cart.init();
-//check to see if there is anything in the cart...
+//test
 if (cart.total > 0) {
   // show our checkout button and total if there are items in the cart.
   document.querySelector('.list-footer').classList.remove('hide');
